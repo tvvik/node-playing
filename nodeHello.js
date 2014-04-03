376ca24226eaa24a3a57690fa01f90030049a840
tvvik <vladlazin@gmail.com>
2014-04-03 05:28:37 +0300

    Hello Server

--- nodeHello.js
+++ nodeHello.js
@@ -1,1 +1,8 @@
 console.log('Hello world!!');
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Worldn');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require("http");
+var url = require("url");
+
+
+/*
+http.createServer(function (request, response) {
+	console.log('Req received! ');
+	response.writeHead(200, {"Content-Type:": "text/plain"});
+	response.write("Hello world!!!!!");
+	response.end();
+}).listen(8888);
+*/
+
+/*
+function onRequest(request, response) {
+	console.log("Request received.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}
+*/
+
+function start(route, handle) {
+	function onRequest(request, response) {
+		var postData = "";
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request received for " + pathname + " received.");
+				
+		route(handle, pathname, response, request);
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+exports.start = start;
